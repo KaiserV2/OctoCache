8eee9e51058b2c1ee76b5c120b42151da757ca41
KaiserV2 <pqchen99@bu.edu>
2023-04-02 23:34:04 +0000

    add hacky_copy

---
+++ hacky_copy.sh
@@ -0,0 +1,20 @@
+sudo ln -sf /mnt/ssd/octomap/lib/liboctomath.so.1.9 /usr/local/lib/liboctomath.so
+sudo ln -sf /mnt/ssd/octomap/lib/liboctomap.so.1.9 /usr/local/lib/liboctomap.so
+                                                                                
+sudo ln -sf /mnt/ssd/octomap/lib/liboctomath.so.1.9 /opt/ros/kinetic/lib/liboctomath.so
+sudo ln -sf /mnt/ssd/octomap/lib/liboctomap.so.1.9 /opt/ros/kinetic/lib/liboctomap.so
+
+sudo ln -sf /mnt/ssd/octomap/lib/liboctomath.so.1.9 /usr/local/lib/liboctomath.so.1.9
+sudo ln -sf /mnt/ssd/octomap/lib/liboctomap.so.1.9 /usr/local/lib/liboctomap.so.1.9
+                                                                                
+SELF_PREFIX=/mnt/ssd/octomap/octomap/include/octomap                    
+ROS_PREFIX=/opt/ros/kinetic/include/octomap                                     
+                                                                                
+sudo cp $SELF_PREFIX/*.h $ROS_PREFIX/                                           
+sudo cp $SELF_PREFIX/*.hxx $ROS_PREFIX/
+mkdir -p $ROS_PREFIX/hash
+mkdir -p $ROS_PREFIX/multi-core
+sudo cp $SELF_PREFIX/hash/*.h $ROS_PREFIX/hash/
+sudo cp $SELF_PREFIX/multi-core/*.h $ROS_PREFIX/multi-core/
+
+# sudo cp $SELF_PREFIX/* $ROS_PREFIX/
